7d8f8bb79ec4eba10f848e93a020b4e3df6b5bd0
Danila <sdanila512@gmail.com>
2020-07-27 22:45:33 +0700

    changed script, modal, added media

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,54 @@ let mySwiper = new Swiper(slider, {
   speed: 400,
 })
 
+const modalClose = document.querySelector('.modal__close'),
+      modal = document.querySelector('.modal'),
+      overlay = document.querySelector('.overlay'),
+      contactsButton = document.querySelector('.contacts__button'),
+      buyButton = document.querySelectorAll('.popular__button-buy'),
+      bookmarkButton = document.querySelectorAll('.popular__button-bookmark'),
+      cartSpan = document.querySelector('.header__cart a span'),
+      bookmarkSpan = document.querySelector('.header__bookmark a span');
+let counterCart = 0,
+    counterBokmark = 0,
+    arrBuy,
+    arrBmark;
+
+console.log(buyButton, bookmarkButton);
+
+
+function overlayDisplay(qwe) {
+  overlay.style.display = qwe;
+}
+
+for (let i = 0; i < buyButton.length; i++) {
+  arrBuy = buyButton[i];
+  arrBuy.addEventListener('click', e => {
+    e.preventDefault();
+    cartSpan.innerHTML = ++counterCart;
+  }) 
+}
+
+for (let i = 0; i < bookmarkButton.length; i++) {
+  arrBmark = bookmarkButton[i];
+  arrBmark.addEventListener('click', e => {
+    e.preventDefault();
+    bookmarkSpan.innerHTML = ++counterBokmark;
+  }) 
+}
+
+contactsButton.addEventListener('click', function(){overlayDisplay('block')});
+
+modalClose.addEventListener('click', function(){overlayDisplay('none')});
+
+overlay.addEventListener('click', function(e){
+  console.log(e.target)
+  if ((e.target !== modal) && (e.target.offsetParent !== modal)) {
+    overlayDisplay('none');
+  }
+});
+  
+
 $(document).ready(function(){
   
   $('ul.service__tabs').on('click', 'li:not(.service__tab_active)', function(){
